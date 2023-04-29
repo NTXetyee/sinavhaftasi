f86905d43f0436249dca73c1cadde96f459878c1
NTXetyee <65107125+NTXetyee@users.noreply.github.com>
2023-04-29 21:40:00 +0300

    tutorial bitti

--- Assets/Scripts/TutorialAnim.cs
+++ Assets/Scripts/TutorialAnim.cs
@@ -1,17 +1,25 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 public class TutorialAnim : MonoBehaviour
 {
-    public float timer;
     [SerializeField] float maxSpeed, smoothSpeed = 0.25f;
     public Transform player, destination;
     public GameObject wasd;
     Vector2 vel = Vector2.zero;
-
+    // WASD harflerinin yavaşça belirmesi için gereken değişkenler
     float smoothTime = 0.3f, smoothVel = 0, currentValue = 0, targetValue = 1;
+
     List<Transform> children;
+
+    // Ana karakterimizin sahneye girme animasyonu
+    void Animation()
+    {
+        player.position = Vector2.SmoothDamp(player.position, destination.position, ref vel, smoothSpeed, maxSpeed);
+    }
+
+
     void Start()
     {
         wasd.SetActive(false);
@@ -27,38 +35,115 @@ public class TutorialAnim : MonoBehaviour
         }
         return children;
     }
+    bool makeYellow = false, dissapear, isEntering = true;
+
     void Update()
     {
-        if (Vector2.Distance(player.position, destination.position) < 0.1f)
+        if (!isEntering)
         {
             wasd.SetActive(true);
             Color color = wasd.GetComponentInChildren<Text>().color;
 
             if (color.a > 0.95f)
             {
-
+                if (!dissapear)
+                {
+                    MakeYellow();
+                    makeYellow = true;
+                    player.GetComponent<PlayerScript>().enabled = true;
+                }
             }
-            else
+            else if (!makeYellow)
             {
+                // WASD harflerinin yavaşça belirme kodu
                 currentValue = Mathf.SmoothDamp(currentValue, targetValue, ref smoothVel, smoothTime);
                 color = new Color(color.r, color.g, color.b, currentValue);
                 wasd.GetComponentInChildren<Text>().color = color;
-
                 foreach (Transform child in children)
                 {
                     child.GetComponent<Text>().color = color;
                 }
             }
-
-
         }
-        else
+        if (Vector2.Distance(player.position, destination.position) < 0.1f)
+        {
+            isEntering = false;
+        }
+        else if (isEntering)
         {
             Animation();
         }
+
+
+
+        if (dissapear)
+        {
+            Dissapear();
+        }
+
+
+        if (congrats)
+        {
+            Congrats();
+        }
     }
-    void Animation()
+    // Eğitim bölümünde hareket tuşlarına basıldığını belirten kod
+    bool w = false, a = false, s = false, d = false;
+    void MakeYellow()
     {
-        player.position = Vector2.SmoothDamp(player.position, destination.position, ref vel, smoothSpeed, maxSpeed);
+        if (Input.GetKeyDown(KeyCode.W) && !w)
+        {
+            GameObject.Find("W").GetComponent<Text>().color = Color.yellow;
+            w = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A) && !a)
+        {
+            GameObject.Find("A").GetComponent<Text>().color = Color.yellow;
+            a = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S) && !s)
+        {
+            GameObject.Find("S").GetComponent<Text>().color = Color.yellow;
+            s = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D) && !d)
+        {
+            GameObject.Find("D").GetComponent<Text>().color = Color.yellow;
+            d = true;
+        }
+        if (w && a && s && d)
+        {
+            dissapear = true;
+            makeYellow = false;
+        }
+    }
+    // WASD harfleri yavaşça kaybolur
+    void Dissapear()
+    {
+        Color color = wasd.GetComponentInChildren<Text>().color;
+        currentValue = Mathf.SmoothDamp(currentValue, 0, ref smoothVel, smoothTime);
+        color = new Color(color.r, color.g, color.b, currentValue);
+        wasd.GetComponentInChildren<Text>().color = color;
+        if (color.a < 0.05f)
+        {
+            foreach (Transform child in children)
+            {
+                child.GetComponent<Text>().color = new Color(0, 0, 0, 0);
+            }
+            congrats = true;
+            dissapear = false;
+        }
+        else
+        {
+            foreach (Transform child in children)
+            {
+                child.GetComponent<Text>().color = color;
+            }
+        }
+    }
+    bool congrats;
+    void Congrats()
+    {
+
     }
 }
